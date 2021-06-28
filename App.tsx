59a5cd05480ffd38102f8aebd709f07e197d2861
elton de campos <eltoncampos36@gmail.com>
2021-06-28 13:03:52 -0300

    chore: add fonts and theme

--- App.tsx
+++ App.tsx
@@ -1,35 +1,35 @@
-import React from 'react';
-import AppLoading from 'expo-app-loading';
+import React from "react";
+import AppLoading from "expo-app-loading";
 
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider } from "styled-components";
 
-import { 
-  useFonts ,
+import {
+  useFonts,
   Poppins_400Regular,
   Poppins_500Medium,
   Poppins_900Black,
-  Poppins_700Bold
-} from '@expo-google-fonts/poppins';
-import theme from './src/global/styles/theme';
-import { Home } from './src/screens/Home';
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
 
+import { PlayfairDisplay_700Bold } from "@expo-google-fonts/playfair-display";
+import theme from "./src/global/styles/theme";
+import { Home } from "./src/screens/Home";
 export default function App() {
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
     Poppins_500Medium,
+    Poppins_700Bold,
     Poppins_900Black,
-    Poppins_700Bold
-  })
+    PlayfairDisplay_700Bold,
+  });
 
   if (!fontsLoaded) {
-    return <AppLoading />
+    return <AppLoading />;
   }
-  
 
   return (
-      <ThemeProvider theme={theme}>
-        <Home/> 
-      </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
   );
 }
-
