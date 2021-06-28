905b2d4288bcb4aaa71ec1ad99ba6c398e896694
elton de campos <eltoncampos36@gmail.com>
2021-06-28 02:13:36 -0300

    feat: create input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useState } from "react";
+
+import * as S from "./styles";
+
+import searchIcon from "../../assets/seachicon.png";
+
+export function Input() {
+  const [value, setValue] = useState("");
+  return (
+    <S.Container>
+      <S.SearchIcon source={searchIcon} />
+      <S.Input
+        value={value}
+        onChangeText={(text) => setValue(text)}
+        placeholder="Search book"
+        placeholderTextColor="#54565A"
+      />
+    </S.Container>
+  );
+}
