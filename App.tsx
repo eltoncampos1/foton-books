438a90b6f7e4a94068e6c8886ccfe4476ee3007e
elton de campos <eltoncampos36@gmail.com>
2021-06-28 00:44:39 -0300

    chore: add fonts

--- App.tsx
+++ App.tsx
@@ -1,21 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
+import { Text } from'react-native'
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+
+import { 
+  useFonts ,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_900Black,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_900Black,
+    Poppins_700Bold
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+  
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <>
+      <Text>OFonton</Text>
       <StatusBar style="auto" />
-    </View>
+      </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/global/styles/theme.ts
@@ -0,0 +1,19 @@
+export default {
+    colors: {
+        background: '#F2F2F2',
+        input_text: '#54565A',
+        primary_text: '#3F4043',
+        secondary_text: '#FF6978',
+        icon_color: '#BFBEBF',
+        selected_icon_color: '#000000',
+        small_text: '#313131',
+        book_title: '#36383A',
+    },
+    fonts: {
+        regular: 'Poppins_400Regular',
+        medium: 'Poppins_500Medium',
+        bold: 'Poppins_700Bold',
+        black : 'Poppins_900Black'
+    }
+}
+
