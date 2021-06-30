166e50c2635e5258294e7b9e041199fb10e7bce2
elton de campos <eltoncampos36@gmail.com>
2021-06-29 22:31:53 -0300

    feat: add status bar

--- App.tsx
+++ App.tsx
@@ -15,6 +15,7 @@ import {
 import { PlayfairDisplay_700Bold } from "@expo-google-fonts/playfair-display";
 import theme from "./src/global/styles/theme";
 import { Home } from "./src/screens/Home";
+import { StatusBar } from "react-native";
 export default function App() {
   const [fontsLoaded] = useFonts({
     Poppins_400Regular_Italic,
@@ -31,6 +32,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar barStyle="light-content" />
       <Home />
     </ThemeProvider>
   );
