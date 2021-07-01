5d774ec740671ce24d9827fcad8680d26a2299b8
elton de campos <eltoncampos36@gmail.com>
2021-07-01 01:29:44 -0300

    style: remove padding right

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -3,7 +3,6 @@ import styled from 'styled-components/native';
 export const Container = styled.ScrollView`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.background};
-  padding-right:20px;
   height: 100%;
 `;
 
