c0eaecf55cd374fa2af1e32801c0a5edcf60c7ce
elton de campos <eltoncampos36@gmail.com>
2021-06-30 22:46:02 -0300

    feat: add Approutes to app component do dealing with routing

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
+import "react-native-gesture-handler";
 import React from "react";
 import AppLoading from "expo-app-loading";
+import { NavigationContainer } from "@react-navigation/native";
 
 import { ThemeProvider } from "styled-components";
 
@@ -14,8 +16,8 @@ import {
 
 import { PlayfairDisplay_700Bold } from "@expo-google-fonts/playfair-display";
 import theme from "./src/global/styles/theme";
-import { Home } from "./src/screens/Home";
 import { StatusBar } from "react-native";
+import { AppRoutes } from "./src/routes/app.routes";
 export default function App() {
   const [fontsLoaded] = useFonts({
     Poppins_400Regular_Italic,
@@ -33,7 +35,9 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <StatusBar barStyle="light-content" />
-      <Home />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
