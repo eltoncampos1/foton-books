6700eab743188393bd632ecc8a97cb8efa42eed3
elton de campos <eltoncampos36@gmail.com>
2021-07-01 01:31:44 -0300

    style: change styles from newBook card component

--- src/components/NewBookCard/styles.ts
+++ src/components/NewBookCard/styles.ts
@@ -1,5 +1,6 @@
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
+import { BorderlessButton } from 'react-native-gesture-handler'
 
 
 
@@ -13,48 +14,76 @@ export const Container = styled.View`
   padding: 15px 20px;
   border-radius: 7px;
   margin-top: ${RFValue(15)}px;
-  opacity: 0.8;
 `;
 export const Description = styled.View`
    width: ${RFValue(144)}px;
+   position: relative;
 `;
 export const BookTitle = styled.Text`
   color: ${({ theme }) => theme.colors.card_text};
 
   font-family: ${({ theme }) => theme.fonts.book_title};
-  font-size: ${RFValue(23)}px;
+  font-size: ${RFValue(22.5)}px;
   margin-bottom: 5px;
 `;
 export const BookAuthor = styled.Text`
   color: ${({ theme }) => theme.colors.card_text};
   font-family: ${({ theme }) => theme.fonts.regular_italic};
 
-  font-size: ${RFValue(13)}px;
+  font-size: ${RFValue(12.5)}px;
   margin-bottom: ${RFValue(25)}px;
 `;
 export const ReadNow = styled.View`
   flex-direction: row;
-  align-items: center;
+  align-items: flex-start;
 `;
 export const Icon = styled.Image`
-  width:${RFValue(18)}px;
+  width:${RFValue(14)}px;
   margin-right: 2px;
 `;
 export const ReadValue = styled.Text`
     color: ${({ theme }) => theme.colors.card_text};
     font-family: ${({ theme }) => theme.fonts.regular};
 
-    font-size: ${RFValue(13)}px;
+    font-size: ${RFValue(10)}px;
 `;
 
 export const ReadValueNumber = styled.Text`
     color: ${({ theme }) => theme.colors.card_text};
     font-family: ${({ theme }) => theme.fonts.bold};
 
-    font-size: ${RFValue(13)}px;
+    font-size: ${RFValue(10)}px;
 `;
+
+export const BookContainer = styled.View``;
+
+export const RedirectButton = styled(BorderlessButton)``;
+
 export const BookThumbnail = styled.Image`
   width: ${RFValue(80)}px;
   height: ${RFValue(115)}px;
   border-radius: 7px;
+`;
+
+
+export const Triangle = styled.Image`
+ position: absolute;
+
+ top: -15px;
+ right: 70px;
+ z-index: 1000;
+ opacity: 1;
+`;
+export const Retangle = styled.Image`
+ position: absolute;
+ bottom: 10%;
+ z-index: 1000;
+ right: 75px;
+`;
+export const Oval = styled.Image`
+  position: absolute;
+  z-index: 1000;
+
+  bottom: 80%;
+  right: 110px;
 `;
