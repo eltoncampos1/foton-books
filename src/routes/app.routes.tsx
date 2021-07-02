82db2fec50b2b329d1739b57b5b922fb7be28f5a
elton de campos <eltoncampos36@gmail.com>
2021-07-02 18:03:19 -0300

    refactor: Homeroutes to approutes

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -4,17 +4,20 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 import { Details } from "../screens/Details";
 import { Search } from "../screens/Search";
+import { Home } from "../screens/Home";
 
-import { HomeRoutes } from "./Home.routes";
+import { AppRoutes } from "./AppRoutes";
+import { Libraries } from "../screens/Libraries";
+import { Profile } from "../screens/Profile";
 
 const Stack = createStackNavigator();
 
 export function StackRoute() {
   return (
-    <Stack.Navigator initialRouteName="Home">
+    <Stack.Navigator>
       <Stack.Screen
         name="Home"
-        component={HomeRoutes}
+        component={Home}
         options={{ headerShown: false }}
       />
       <Stack.Screen
@@ -25,6 +28,16 @@ export function StackRoute() {
       <Stack.Screen
         name="Details"
         component={Details}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Libraries"
+        component={Libraries}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Profile"
+        component={Profile}
         options={{ headerShown: false }}
       />
     </Stack.Navigator>
