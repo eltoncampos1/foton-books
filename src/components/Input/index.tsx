974289116da2f1d6567e54be579b4f6d6d6ba51e
elton de campos <eltoncampos36@gmail.com>
2021-07-02 11:50:03 -0300

    feat: create handleInputChange functio to make redirect to search page passaing de value with params

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -4,9 +4,16 @@ import { useState } from "react";
 import * as S from "./styles";
 
 import searchIcon from "../../assets/seachicon.png";
+import { useNavigation } from "@react-navigation/native";
 
 export function Input() {
   const [value, setValue] = useState("");
+  const navigation = useNavigation();
+
+  function handleInputChange() {
+    navigation.navigate("Search", { InputParam: value });
+    setValue("");
+  }
 
   return (
     <S.Container>
@@ -16,6 +23,7 @@ export function Input() {
         onChangeText={(text) => setValue(text)}
         placeholder="Search book"
         placeholderTextColor="#54565A"
+        onSubmitEditing={handleInputChange}
       />
     </S.Container>
   );
