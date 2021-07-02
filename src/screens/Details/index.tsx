211a415af0bce1155bd24ac32e0df8312a44c5b4
elton de campos <eltoncampos36@gmail.com>
2021-07-02 01:36:11 -0300

    feat: create details page

--- src/screens/Details/index.tsx
+++ src/screens/Details/index.tsx
@@ -1,6 +1,33 @@
 import React from "react";
-import { Text } from "react-native";
 
-export function Details() {
-  return <Text>Details</Text>;
+import * as S from "./styles";
+
+import detailbg from "../../assets/bgDetails.png";
+import detailmask from "../../assets/detailmask.png";
+import detailmask1 from "../../assets/demailmask1.png";
+import detailmask2 from "../../assets/detailmask2.png";
+import detailmask3 from "../../assets/detailmask3.png";
+
+export function Details({ route }: any) {
+  const { bookName, bookSubtitle, bookAuthors, bookDescription, bookThumb } =
+    route.params;
+
+  return (
+    <S.Container>
+      <S.TopBackgroundImage source={detailbg}>
+        <S.DetailMask source={detailmask} />
+        <S.DetailMask1 source={detailmask1} />
+        <S.DetailMask2 source={detailmask2} />
+        <S.DetailMask3 source={detailmask3} />
+        <S.BookThumbnail source={{ uri: bookThumb }} />
+      </S.TopBackgroundImage>
+
+      <S.TitleContainer>
+        <S.BookTitle>{bookName} :</S.BookTitle>
+        <S.BookSubTitle>{bookSubtitle}</S.BookSubTitle>
+      </S.TitleContainer>
+      <S.BookAuthors>{bookAuthors}</S.BookAuthors>
+      <S.BookDescription>{bookDescription}</S.BookDescription>
+    </S.Container>
+  );
 }
---
+++ src/screens/Details/styles.ts
@@ -0,0 +1,73 @@
+import { RFValue } from 'react-native-responsive-fontsize';
+import styled from 'styled-components/native';
+
+export const Container = styled.ScrollView`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background};
+`;
+export const TopBackgroundImage = styled.ImageBackground`
+  width: ${RFValue(376)}px;
+  height: ${RFValue(282)}px;
+
+  position: relative;
+`;
+export const BookThumbnail = styled.Image`
+  width: ${RFValue(151)}px;
+  height: ${RFValue(234)}px;
+  position: absolute;
+  bottom: -40px;
+  right:  ${RFValue(111)}px;
+`;
+export const TitleContainer = styled.Text`
+  padding-left:  ${RFValue(20)}px;
+  margin-top:  ${RFValue(67)}px;
+  margin-bottom: ${RFValue(10)}px;
+`;
+export const BookTitle = styled.Text`
+   font-family: ${({ theme }) => theme.fonts.bold};
+  color: ${({ theme }) => theme.colors.book_title};
+  font-size: ${RFValue(24)}px;
+`;
+export const BookSubTitle = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  color: ${({ theme }) => theme.colors.book_title};
+  font-size: ${RFValue(24)}px;
+`;
+export const BookAuthors = styled.Text`
+  padding-left:  ${RFValue(20)}px;
+  font-family: ${({ theme }) => theme.fonts.medium};
+  color: ${({ theme }) => theme.colors.secondary_text};
+  font-size: ${RFValue(16)}px;
+  margin-bottom: ${RFValue(10)}px;
+
+`;
+export const BookDescription = styled.Text`
+    padding-left:  ${RFValue(20)}px;
+    font-family: ${({ theme }) => theme.fonts.medium};
+    color: ${({ theme }) => theme.colors.small_text};
+    font-size: ${RFValue(16)}px;
+    padding-right:  ${RFValue(20)}px;
+
+
+    opacity: 0.6;
+`;
+export const DetailMask = styled.Image`
+   position: absolute;
+   top: 0;
+   right: 0;
+`;
+export const DetailMask1 = styled.Image`
+   position: absolute;
+   bottom: 10px;
+   right: 95px;
+`;
+export const DetailMask2 = styled.Image`
+   position: absolute;
+   bottom: 120px;
+   left: 85px;
+`;
+export const DetailMask3 = styled.Image`
+   position: absolute;
+   top: 125px;
+   left: 60px;
+`;
