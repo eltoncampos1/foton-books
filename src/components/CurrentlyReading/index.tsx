a39ccb98326dc161f4a6b75fec88054651b64a86
elton de campos <eltoncampos36@gmail.com>
2021-07-02 18:06:38 -0300

    feat: make curentlyreading component redirect to details page passim params

--- src/components/CurrentlyReading/index.tsx
+++ src/components/CurrentlyReading/index.tsx
@@ -1,35 +1,57 @@
 import React from "react";
-import * as S from "./styles";
+import { useNavigation } from "@react-navigation/native";
+import { BorderlessButtonProps } from "react-native-gesture-handler";
 
 import BookIcon from "../../assets/bookIcon.png";
 import BAckImage from "../../assets/bg-currently.png";
 
-interface CurrentlyReadingprops {
+import * as S from "./styles";
+
+interface CurrentlyReadingprops extends BorderlessButtonProps {
   bookTitle: string;
-  bookAuthor: string;
+  bookAuthors: any;
   bookThumbnail: string;
+  bookSubtitle?: string;
+  bookDescription?: string;
+  onPress?: () => void;
 }
 
 export function CurrentlyReading({
   bookTitle,
-  bookAuthor,
+  bookAuthors,
   bookThumbnail,
+  bookSubtitle,
+  bookDescription,
 }: CurrentlyReadingprops) {
+  const navigation = useNavigation();
+
+  function handleClick() {
+    navigation.navigate("Details", {
+      bookThumb: bookThumbnail,
+      bookName: bookTitle,
+      bookSubtitle: bookSubtitle,
+      bookAuthors: bookAuthors,
+      bookDescription: bookDescription,
+    });
+  }
+
   return (
-    <S.Container source={BAckImage}>
-      <S.Book source={{ uri: bookThumbnail }} />
+    <S.BookWrapper onPress={handleClick}>
+      <S.Container source={BAckImage}>
+        <S.Book source={{ uri: bookThumbnail }} />
 
-      <S.BookDescription>
-        <S.BookTitle>{bookTitle}</S.BookTitle>
-        <S.BookAuthor>by {bookAuthor}</S.BookAuthor>
+        <S.BookDescription>
+          <S.BookTitle>{bookTitle}</S.BookTitle>
+          <S.BookAuthor>by {bookAuthors}</S.BookAuthor>
 
-        <S.ChapterDescription>
-          <S.BookIcon source={BookIcon} />
-          <S.Chapter>Chapter </S.Chapter>
-          <S.ChapterNumber>2 </S.ChapterNumber>
-          <S.ChapterFrom>From 9</S.ChapterFrom>
-        </S.ChapterDescription>
-      </S.BookDescription>
-    </S.Container>
+          <S.ChapterDescription>
+            <S.BookIcon source={BookIcon} />
+            <S.Chapter>Chapter </S.Chapter>
+            <S.ChapterNumber>2 </S.ChapterNumber>
+            <S.ChapterFrom>From 9</S.ChapterFrom>
+          </S.ChapterDescription>
+        </S.BookDescription>
+      </S.Container>
+    </S.BookWrapper>
   );
 }
